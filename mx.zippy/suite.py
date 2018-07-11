9e3324d6aea0327fe499b9e07b1a67194ddd1db3
Mohaned <mohioq@gmail.com>
2018-07-11 04:12:58 +0300

    [MG] update MegaGuards

--- mx.zippy/suite.py
+++ mx.zippy/suite.py
@@ -17,7 +17,7 @@ suite = {
             {
                "name" : "megaguards",
                "subdir" : False,
-               "version" : "1d822342eb9774b19154df7d345d5a0dcc6d7898",
+               "version" : "8064db26e1a8a41871048fbf58be965a62690d60",
                "urls" : [
                     {"url" : "git@github.com:securesystemslab/megaguards.git", "kind" : "git"},
                 ]
